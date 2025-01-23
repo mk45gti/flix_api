6187a0b453c10ae3772d97ea652f2ba70597a10a
Diego Fávero <diego@segmarket.com.br>
2025-01-23 20:34:01 -0300

    Atualização de Serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from movies.models import Movie
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser inferior a 1990.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) < 100:
+            raise serializers.ValidationError('A sinopse deve ter no mínimo 100 caracteres.')
+        return value
