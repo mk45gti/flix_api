6b7c2dc087c0cc00185a9820f0cfc2f2f2e67223
Diego Fávero <diego@segmarket.com.br>
2025-01-24 12:53:09 -0300

    Implantação do JWT nas views

--- genres/views.py
+++ genres/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from genres.serializers import GenreSerializer
 
 class GenereCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
