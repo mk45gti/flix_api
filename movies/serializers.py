f44d75d28d3713e1145956d9c93cf5818fc70102
Diego Fávero <diego@segmarket.com.br>
2025-01-24 09:28:04 -0300

    Adicionando o calculo de rate no movie

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 from movies.models import Movie
+from django.db.models import Avg
 
 class MovieModelSerializer(serializers.ModelSerializer):
-    
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        return None
 
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1970:
             raise serializers.ValidationError('A data de lançamento não pode ser inferior a 1990.')
         return value
     
