69be7917881d6d67bbd33db7bdf5e053c1cbb082
Diego Fávero <diego@segmarket.com.br>
2025-01-25 10:30:56 -0300

    Adicionando permissoes no app genres

--- genres/views.py
+++ genres/views.py
@@ -3,14 +3,15 @@ from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from genres.serializers import GenreSerializer
+from genres.permissions import GenrePermissionClass
 
 class GenereCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,GenrePermissionClass,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
