4e557956ad73c5604ca4c93e6b7f749cd6d70726
Diego Fávero <diego@segmarket.com.br>
2025-01-25 17:30:50 -0300

    Implantação das permissoes globais

--- genres/views.py
+++ genres/views.py
@@ -1,17 +1,18 @@
 from rest_framework import generics
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from genres.models import Genre
 from genres.serializers import GenreSerializer
-from genres.permissions import GenrePermissionClass
+
 
 class GenereCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GenrePermissionClass,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,GenrePermissionClass,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
